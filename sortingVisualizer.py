adf2c4e0b8ba01dd591af6fc053c4bbcf3a992b4
Izae21 <107613096+izae21@users.noreply.github.com>
2022-09-08 22:15:59 -0700

    Updated draw functions, implemented bubble sort, basic visualizer works

--- sortingVisualizer.py
+++ sortingVisualizer.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 
 # Use a class to store information, instead of creating global variables
@@ -12,9 +13,10 @@ class valuesAndInformation:
   LARGER_FONT = pygame.font.SysFont('cambria', 35)
   # RGB Values
   WHITE = 248, 248, 255
-  RED = 255, 0, 0
-  GREEN = 0, 255, 0
+  RED = 238, 0, 0
+  GREEN = 84, 255, 159
   BLUE = 0, 104, 139
+  YELLOW = 255, 193, 37
   BACKGROUND = BLUE
   # Gradient colors for yellowish gold
   GRADIENTCOLORS = [(255,193,37), (238,180,34), (205,155,29)]
@@ -38,7 +40,7 @@ class valuesAndInformation:
     # For example, if there are 100 values, the heights will be smaller than if there were only 10 values.
     # Object of these calculations is to find the total "drawable" area.
     self.blockWidth = round((self.width - self.SIDE_PADDING)) / len(aList)
-    self.blockHeight = round((self.height - self.TOP_PADDING) / (self.maximumValue - self.minimumValue))
+    self.blockHeight = math.floor((self.height - self.TOP_PADDING) / (self.maximumValue - self.minimumValue))
 
     # We want to start at bottom left, in pygame coordinates start at top left aka (0,0)
     # As you move down, Y increase. As you move right, X increases.
@@ -55,27 +57,41 @@ def generate_starting_list(n, minimum, maximum):
 
   return displayList
 
+
 """ DRAW LIST TO DISPLAY TO WINDOW """
-def drawDisplay(drawInfo):
+def drawDisplay(drawInfo, algoName, ascend):
   # Everytime we draw, we want to overwrite what was previously on display with a background color.
   # This way there won't be any overlap visible to the user
   drawInfo.window.fill(drawInfo.BACKGROUND)
+  
+  # Draw title:
+  title = drawInfo.LARGER_FONT.render(f"{algoName} - {'Ascending' if ascend else 'Descending'}", 1, drawInfo.YELLOW)
+  drawInfo.window.blit(title, (drawInfo.width/2 - title.get_width()/2, 5))
 
   # Draw controls:
   controls = drawInfo.FONT.render("R - Reset || S - Sort || A - Ascending || D - Descending", 1, drawInfo.WHITE)
-  drawInfo.window.blit(controls, (drawInfo.width/2 - controls.get_width()/2, 5))
+  drawInfo.window.blit(controls, (drawInfo.width/2 - controls.get_width()/2, 45))
 
   # Draw Possible Sorting Choices:
   sortChoice = drawInfo.FONT.render("B - Bubble Sort || I - Insertion Sort", 1, drawInfo.WHITE)
-  drawInfo.window.blit(sortChoice, (drawInfo.width/2 - sortChoice.get_width()/2, 35))
+  drawInfo.window.blit(sortChoice, (drawInfo.width/2 - sortChoice.get_width()/2, 75))
 
   drawList(drawInfo)
   pygame.display.update()
   return 
 
-def drawList(drawInfo):
+def drawList(drawInfo, colorPos={}, clearBackground=False):
   # Draw the rectangles for each of the elements in the list
   lst = drawInfo.aList
+
+  # Get the area that the replaced block will need without redrawing entire window
+  if clearBackground:
+    clearRectangle = (drawInfo.SIDE_PADDING//2, drawInfo.TOP_PADDING,
+                      drawInfo.width - drawInfo.SIDE_PADDING,
+                      drawInfo.height - drawInfo.TOP_PADDING)
+
+    pygame.draw.rect(drawInfo.window, drawInfo.BACKGROUND, clearRectangle)
+
   # Get index and value of each element. Then calculate x and y coordinate
   for i, val in enumerate(lst):
     x = drawInfo.startingX + i * drawInfo.blockWidth
@@ -83,31 +99,71 @@ def drawList(drawInfo):
     # Every 3 elements switch to another shade
     colorUsed = drawInfo.GRADIENTCOLORS[i % 3]
 
+    # Override the color for the block that is being sorted (to highlight it)
+    if i in colorPos:
+      colorUsed = colorPos[i]
+
     pygame.draw.rect(drawInfo.window, colorUsed, (x, y, drawInfo.blockWidth, drawInfo.height))
+
+  # Update the screen for the area of the cleared rectangle
+  if clearBackground:
+    pygame.display.update()
 
   return
 
-    
+
+""" SORTING FUNCTIONS """
+# Bubble Sort: Time Complexity: O(n^2)
+def bubbleSort(drawInfo, ascend=True):
+  inputList = drawInfo.aList
+  
+  for i in range(len(inputList) - 1):
+    for j in range(len(inputList) - i - 1):
+      n1 = inputList[j]
+      n2 = inputList[j + 1]
+
+      if (n1 > n2 and ascend) or (n1 < n2 and not ascend):
+        inputList[j], inputList[j + 1] = inputList[j + 1], inputList[j]
+        drawList(drawInfo, {j: drawInfo.GREEN, j+1: drawInfo.RED}, True)
+        # this generator allows us to still have controls while iteration occurs
+        yield True
+
+  return inputList
 
 
-# MAIN DRIVER FUNCTION:
+
+
+""" MAIN DRIVER FUNCTION """
 def main():
   run = True
   clock = pygame.time.Clock()
 
   nowSort = False
-  ascend = False
-  descend = False
+  ascend = True
+
+  sortAlgo = bubbleSort
+  sortAlgoName = "Bubble Sort"
+  sortAlgoGenerator = None
+
   n, minimumVal, maximumVal = 43, 0, 100
 
   displayList = generate_starting_list(n, minimumVal, maximumVal)
-  drawValuesAndInfo = valuesAndInformation(900, 700, displayList)
+  drawInfo = valuesAndInformation(900, 700, displayList)
 
   while run:
     # Max number of time loop can run per second
-    clock.tick(60)
-    # Render display to window
-    drawDisplay(drawValuesAndInfo)
+    clock.tick(70)
+
+    # Use the generator to get to next yield, end when no more items to iterate
+    if nowSort:
+      try:
+        next(sortAlgoGenerator)
+      except StopIteration:
+        nowSort = False
+    else:
+      # Render display to window
+      drawDisplay(drawInfo, sortAlgoName, ascend)
+
 
     for event in pygame.event.get():
       # When user quits application
@@ -120,13 +176,15 @@ def main():
         continue
       if event.key == pygame.K_r:
         displayList = generate_starting_list(n, minimumVal, maximumVal)
-        drawValuesAndInfo.set_list(displayList)
+        drawInfo.set_list(displayList)
         # Reset sorting when list is reset
         nowSort = False
 
       # Initialize sorting on key press and if not already sorting
       elif event.key == pygame.K_s and nowSort == False:
         nowSort = True
+        sortAlgoGenerator = sortAlgo(drawInfo, ascend)
+
       # Let user decide to sort by ascending or descending order
       elif event.key == pygame.K_a and not nowSort:
         ascend = True
